faa0779c964ac8d8cf563ab7538d906d280354ad
betque <quentin.bette@gmail.com>
2020-11-18 17:07:39 +0100

    Adding new meteo data (partial)

---
+++ src/meteo.py
@@ -0,0 +1,33 @@
+import requests
+import pandas as pd
+import re
+
+
+def download_all_data(start_year, end_year):
+    """Downloading all meteo data files from start_year to end_year"""
+    years = range(start_year, end_year+1)
+    months = range(1, 13)
+    for year in years:
+        for month in months:
+            date = str(year)+str(month).zfill(2)
+            url = "https://donneespubliques.meteofrance.fr/donnees_libres/Txt/Synop/Archive/synop."+date+".csv.gz"
+            r = requests.get(url, allow_redirects=True)
+            if "DOCTYPE HTML" not in str(r.content):
+                open("../data/synop/"+date+".csv", 'wb').write(r.content)
+                print(date)
+                print(r.content)
+
+
+def load_from_csv(path):
+    """Create a dataframe from csv file, and dropping last column (there is an unwanted ; at the end of each line)"""
+    df = pd.read_csv(path, sep=";")
+    df.drop(df.columns[len(df.columns)-1], axis=1, inplace=True)
+    return df
+
+
+if __name__ == "__main__":
+    #download_all_data(1990, 1996)
+    folder = "../data/synop/"
+    df = load_from_csv(folder+"202006.csv")
+    print(df.head())
+    print(df["numer_sta"].unique().tolist())
