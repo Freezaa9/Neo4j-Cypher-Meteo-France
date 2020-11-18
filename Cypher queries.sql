764c83a53257391c589b7ff1871b1b23c933bcbe
Geoffrey HARRAZI <geoffreyharrazi@gmail.com>
2020-11-18 19:28:58 +0100

    python notebook get city location

--- Cypher queries.sql
+++ Cypher queries.sql
@@ -10,7 +10,7 @@ CREATE CONSTRAINT ON (j:Jour) ASSERT j.numero IS UNIQUE
 
 LOAD CSV WITH HEADERS FROM 'file:///202011.csv' AS line FIELDTERMINATOR ';'
 MERGE  (v:Ville { numer_sta: toString(line.numer_sta)})
-MERGE  (r:Releve { t: toInteger(line.t), u: toInteger(line.u)})
+CREATE  (r:Releve { t: toInteger(line.t), u: toInteger(line.u)})
 MERGE  (m:Mois { numero: substring(toString(line.date), 4, 2)})
 MERGE  (a:Annee { numero: substring(toString(line.date), 0, 4)})
 MERGE  (j:Jour { numero: substring(toString(line.date), 6, 2)})
@@ -26,9 +26,9 @@ CREATE (r)-[z:a_lheure]->(h)
 LOAD CSV WITH HEADERS FROM 'file:///postesSynop.csv' AS line FIELDTERMINATOR ';'
 MERGE (v:Ville {numer_sta: line.ID})
 SET v.Nom = line.Nom
-SET v.Latitude = toInteger(line.Latitude)
-SET v.Longitude = toInteger(line.Longitude)
-SET v.Altitude = toInteger(line.Altitude)
+SET v.Latitude = toString(line.Latitude)
+SET v.Longitude = toString(line.Longitude)
+SET v.Altitude = toString(line.Altitude)
 
 
 
