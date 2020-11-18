fe8990102e555f7f0c8e32af0e739487cfc84889
Geoffrey HARRAZI <geoffreyharrazi@gmail.com>
2020-11-18 18:03:20 +0100

    new image and create query fro synop

--- Cypher queries.sql
+++ Cypher queries.sql
@@ -1,19 +1,27 @@
 
 
 
-CREATE CONSTRAINT ON (v:Ville) ASSERT v.NUM_POSTE IS UNIQUE
-CREATE CONSTRAINT ON (m:Mois) ASSERT m.MOIS IS UNIQUE
-CREATE CONSTRAINT ON (a:Annee) ASSERT a.ANNEE IS UNIQUE
+CREATE CONSTRAINT ON (v:Ville) ASSERT v.numer_sta IS UNIQUE
+CREATE CONSTRAINT ON (m:Mois) ASSERT m.numero IS UNIQUE
+CREATE CONSTRAINT ON (a:Annee) ASSERT a.numero IS UNIQUE
+CREATE CONSTRAINT ON (j:Jour) ASSERT j.numero IS UNIQUE
+CREATE CONSTRAINT ON (j:Jour) ASSERT j.numero IS UNIQUE
 
 
-LOAD CSV WITH HEADERS FROM 'file:///climat2019TO2020.csv' AS line FIELDTERMINATOR ','
-MERGE  (v:Ville { NUM_POSTE: toString(line.NUM_POSTE)})
-CREATE  (r:Releve { PSTATM: toInteger(line.PSTATM), PMERM: toInteger(line.PMERM)})
-MERGE  (m:Mois { MOIS: line.MOIS})
-MERGE  (a:Annee { ANNEE: line.ANNEE})
-CREATE (r)-[w:depuis]->(v)
-CREATE (r)-[x:de_lannee]->(a)
-CREATE (r)-[c:du_mois]->(m)
+LOAD CSV WITH HEADERS FROM 'file:///202011.csv' AS line FIELDTERMINATOR ';'
+MERGE  (v:Ville { numer_sta: toString(line.numer_sta)})
+CREATE  (r:Releve { t: toInteger(line.t), h: toInteger(line.h)})
+MERGE  (m:Mois { numero: substring(line.date, 4, 5)})
+MERGE  (a:Annee { numero: left(line.date,3)})
+MERGE  (j:Jour { numero: substring(line.date, 6, 7)})
+MERGE  (h:Heure { numero: substring(line.date, 8, 9)})
+CREATE (r)-[t:depuis]->(v)
+CREATE (r)-[w:de_lannee]->(a)
+CREATE (r)-[x:du_mois]->(m)
+CREATE (r)-[y:le_jour]->(j)
+CREATE (r)-[z:a_lheure]->(h)
+
+
 
 LOAD CSV WITH HEADERS FROM 'file:///postesSynop.csv' AS line FIELDTERMINATOR ';'
 MERGE (v:Ville {NUM_POSTE: line.ID})
@@ -27,5 +35,6 @@ ON MATCH SET v.NUM_POSTE = toString(line.ID)
 
 MATCH (n)
 DETACH DELETE n
+
 
 
